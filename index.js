a8972a99d244dffec77e9f2a7985d384478f9730
Rupar Kathi <murshedatasnim@gmail.com>
2019-11-15 03:04:11 +0600

    2nd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("hello");
+var a = 9;
+var c = "okay";
+let s = "kajshd";//can be used only within a scope
+const k = 133; //value can't be changed
+console.log(a);
+console.log(c);
+console.log(s);
+console.log(k);
+// k = s;
+// console.log(k);
---
+++ readme.txt
@@ -0,0 +1,8 @@
+to run
+ctrl + shift+b
+
+
+to clone from github
+shift+ ctrl + p
+search for github clone
+paste repo link
